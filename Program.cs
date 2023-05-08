a707ebf4fbe214adfb9db7ab58d877da6203ce6a
Kelsei Kyser <kkelsei127@yahoo.com>
2023-05-08 17:11:56 -0400

    hey, I can write to the command line in C#

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and Welcome to CatWorx, all things needed for Cattios!");
+        }
+    }
+}
