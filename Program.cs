7ed7cc578cc6149577a7f17768aa35c8211342b0
Kelsei Kyser <kkelsei127@yahoo.com>
2023-05-08 20:27:38 -0400

    added get name and print name functionality

--- Program.cs
+++ Program.cs
@@ -2,14 +2,33 @@
 // Console.WriteLine("Hello, World!");
 
 using System;
+using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
+
 {
     class Program
     {
         static void Main(string[] args)
+        
         {
-            Console.WriteLine("Hello and Welcome to CatWorx, all things needed for Cattios!");
+            List<string> employees = new List<string>();
+            while (true){
+                Console.WriteLine("Please enter a CatWorx employee name: ");
+            
+                string input = Console.ReadLine() ?? "";
+                // the "??" is called a null coalescing operator 
+                    //if null it returns an empty string
+
+                // Break if the user hits ENTER without typing a name
+                if (input == "") {break;}
+
+                employees.Add(input);
+            }
+            
+            for (int i = 0; i < employees.Count; i++ ){
+                Console.WriteLine(employees[i]);
+            }
         }
     }
 }
